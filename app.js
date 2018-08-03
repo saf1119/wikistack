a9490b50ee861cbfacce122577efb5348ae1056a
pindimindi <milicarodic307@yahoo.com>
2018-08-03 10:10:40 -0500

    serever not working

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({extended: false}));
+
+app.get("/", (req, res) => {
+    res.send("Hello!!");
+})
+
