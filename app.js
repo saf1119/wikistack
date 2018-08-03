29498f329b1b32ebd24f3aa20ea86606c48c1cfa
Sam Fortuna <saf1119@gmail.com>
2018-08-03 10:43:22 -0500

    Sequelize setup

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const models = require('./models');
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
@@ -9,4 +10,19 @@ app.use(express.urlencoded({extended: false}));
 app.get("/", (req, res) => {
     res.send("Hello!!");
 })
+
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
+
+const init = async () => {
+	await models.db.sync({force: true})
+	const PORT = 3000;
+	app.listen(PORT, () => {
+		console.log(`app listening on port ${PORT}`)
+	})
+}
+
+init();
 
